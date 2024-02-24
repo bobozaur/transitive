9fdfaa97f2285d45bf04e2f76a88a57dd49feab1
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2024-02-24 12:08:45 +0200

    Added Into generics tests

--- tests/into.rs
+++ tests/into.rs
@@ -1,5 +1,7 @@
 mod macros;
 
+use std::marker::PhantomData;
+
 use transitive::Transitive;
 
 #[derive(Transitive)]
@@ -7,7 +9,7 @@ use transitive::Transitive;
 struct A;
 
 #[derive(Transitive)]
-#[transitive(into(C, D))] // impl From<B> for  D
+#[transitive(into(C, D))] // impl From<B> for D
 struct B;
 struct C;
 struct D;
@@ -16,8 +18,57 @@ impl_from!(A to B);
 impl_from!(B to C);
 impl_from!(C to D);
 
+#[derive(Transitive)]
+#[transitive(into(A, B, C, D))] // impl From<Z<T>> for D
+struct Z<T>(PhantomData<T>);
+
+impl<T> From<Z<T>> for A {
+    fn from(_value: Z<T>) -> Self {
+        Self
+    }
+}
+
+#[derive(Transitive)]
+#[transitive(into(A, B, C, D))] // impl From<Y<'a>> for D
+struct Y<'a>(PhantomData<&'a ()>);
+
+impl<'a> From<Y<'a>> for A {
+    fn from(_value: Y<'a>) -> Self {
+        Self
+    }
+}
+
+#[derive(Transitive)]
+#[transitive(into(A, B, C, D))] // impl From<W<N>> for D
+struct W<const N: usize>;
+
+impl<const N: usize> From<W<N>> for A {
+    fn from(_value: W<N>) -> Self {
+        Self
+    }
+}
+
+#[derive(Transitive)]
+#[transitive(into(A, B, C, D))] // impl From<Q<'a, 'b, N, T, U>> for D
+struct Q<'a, 'b: 'a, const N: usize, T: 'a + Send, U: 'b>(PhantomData<(&'a T, &'b U)>)
+where
+    T: Sync;
+
+impl<'a, 'b: 'a, const N: usize, T: 'a + Send, U: 'b> From<Q<'a, 'b, N, T, U>> for A
+where
+    T: Sync,
+{
+    fn from(_value: Q<'a, 'b, N, T, U>) -> Self {
+        Self
+    }
+}
+
 #[test]
 pub fn test_into() {
     let _ = D::from(A);
     let _ = D::from(B);
+    let _ = D::from(Z(PhantomData::<()>));
+    let _ = D::from(Y(PhantomData));
+    let _ = D::from(W::<2>);
+    let _ = D::from(Q::<2, (), ()>(PhantomData));
 }
