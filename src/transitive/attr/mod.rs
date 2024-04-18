d0909c19dbb11beb91f0e260496110ddc8d234ae
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2024-04-18 09:45:49 +0300

    Improved TransitiveAttr::try_from

--- src/transitive/attr/mod.rs
+++ src/transitive/attr/mod.rs
@@ -1,7 +1,6 @@
 mod parsed;
 
 use darling::FromAttributes;
-use quote::ToTokens;
 use syn::Attribute;
 
 use super::{
@@ -22,20 +21,20 @@ impl TryFrom<Attribute> for TransitiveAttr {
     type Error = darling::Error;
 
     fn try_from(value: Attribute) -> Result<Self, Self::Error> {
-        match value.path().to_token_stream().to_string().as_str() {
-            "transitive_from" => {
-                TransitiveFrom::from_attributes(&[value]).map(TransitiveAttr::From)
-            }
-            "transitive_into" => {
-                TransitiveInto::from_attributes(&[value]).map(TransitiveAttr::Into)
-            }
-            "transitive_try_from" => {
-                TransitiveTryFrom::from_attributes(&[value]).map(TransitiveAttr::TryFrom)
-            }
-            "transitive_try_into" => {
-                TransitiveTryInto::from_attributes(&[value]).map(TransitiveAttr::TryInto)
-            }
-            p => Err(darling::Error::unsupported_shape(p)),
+        let Some(ident) = value.path().get_ident() else {
+            return Err(darling::Error::missing_field("attribute name"));
+        };
+
+        if ident == "transitive_from" {
+            TransitiveFrom::from_attributes(&[value]).map(TransitiveAttr::From)
+        } else if ident == "transitive_into" {
+            TransitiveInto::from_attributes(&[value]).map(TransitiveAttr::Into)
+        } else if ident == "transitive_try_from" {
+            TransitiveTryFrom::from_attributes(&[value]).map(TransitiveAttr::TryFrom)
+        } else if ident == "transitive_try_into" {
+            TransitiveTryInto::from_attributes(&[value]).map(TransitiveAttr::TryInto)
+        } else {
+            Err(darling::Error::unsupported_shape(&ident.to_string()))
         }
     }
 }
